83426944ba3d77b11539d078aa727262cce10194
Tameem Hossain <t23hossa@uwaterloo.ca>
2021-11-24 16:35:27 -0500

    Comments and slight fixes.

--- src/main.c
+++ src/main.c
@@ -1,25 +1,19 @@
-// Sample code for ECE 198
+// Code for ECE 198 Project- PATTERN MATCHER
 
-// Written by Bernie Roehl, August 2021
-
-// This file contains code for a number of different examples.
-// Each one is surrounded by an #ifdef ... #endif block inside of main().
+// Written by Tameem Hossain and Tahmid Ahmed, November 2021
+// with sample code help from Bernie Roehl, August 2021
 
 // To run a particular example, you should remove the comment (//) in
-// front of exactly ONE of the following lines:
+// front of exactly ONE of the following lines below.
+// Only the examples that the game used concepts for (from the original sample code) remain and are commented out for reference.
+// the defined PATTERN_MATCH is however our own game code made by Tameem and Tahmid.  
 
-#define PATTERN_MATCH   // our own function to be implemented for the game.
-//#define BUTTON_BLINK    //--> concepts to be used
-//#define LIGHT_SCHEDULER   //--> concepts to be used
-//#define TIME_RAND
-// #define KEYPAD   --> concepts to be used
-//#define KEYPAD_CONTROL    --> concepts to be used
-// #define SEVEN_SEGMENT
-// #define KEYPAD_SEVEN_SEGMENT
-// #define COLOR_LED
-// #define ROTARY_ENCODER
-// #define ANALOG
-// #define PWM
+#define PATTERN_MATCH   // Main game code that uses loops to show success at each level, and whether they have successfully unlocked the lock or not.
+//#define BUTTON_BLINK    --> concepts were used
+//#define LIGHT_SCHEDULER   --> concepts were used
+//#define TIME_RAND    --> concepts were used
+// #define KEYPAD   --> concepts were used
+//#define KEYPAD_CONTROL    --> concepts were used
 
 #include <stdbool.h> // booleans, i.e. true and false
 #include <stdio.h>   // sprintf() function
@@ -40,7 +34,7 @@ int sequence_lengthGENERATOR() { //sequence will be between 8-12 in length.
 
 int * rand_output_generation(int size);
 int * rand_output_generation(int size) { //assigns the sequence of indexes of ports at which light will be flashed
-    while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));
+    while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  //awaits blue button press as mentioned in the serial port.
     srand(HAL_GetTick());
     int * array;
     array = malloc(size); //allocating the new array pointer.
@@ -94,7 +88,7 @@ void output_by_LED(int LED_indx) {
 }
 
 bool compare(int outputs [], int inputs [], int currIndx);
-bool compare(int outputs [], int inputs [], int currIndx) {
+bool compare(int outputs [], int inputs [], int currIndx) { //compares input array to output array after the end of input event handling. 
     if (outputs[currIndx]==inputs[currIndx]) {
         return true;
     } else {
@@ -117,7 +111,7 @@ bool level(int lvl_num) {  //main code for one level iteration
     //output the lights (using pins and ports)
     //arrange difficulty time of output using similar code to LIGHT_SCHEDULER
     int * outputIndx_Arr;
-    outputIndx_Arr = malloc(num_elements);
+    outputIndx_Arr = malloc(num_elements); //allocation of outputs
     outputIndx_Arr = rand_output_generation(num_elements);
     for(int i=0; i<num_elements; i++) {
         output_by_LED(outputIndx_Arr[i]);
@@ -128,11 +122,11 @@ bool level(int lvl_num) {  //main code for one level iteration
 
     //input array declarations.
     int *elements;//main array for keypad input elements.
-    elements = malloc(num_elements); 
+    elements = malloc(num_elements);  //allocation of inputs.
 
     //input generation begins.
     SerialPuts("\n(Press blue button on board to start inputs)\n");
-    while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13)); //
+    while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13)); //await blue button press as stated by the serial port.
     InitializeKeypad(); // initializes the keypad for inputs
     while (true)
     {
@@ -153,7 +147,7 @@ bool level(int lvl_num) {  //main code for one level iteration
                     HAL_Delay(500);  // 250 milliseconds == 1/4 second
                     r++;
                     if(r==5 && HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5) == 0) {
-                        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, false);
+                        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, false);  //makes sure nothing else faultly flashes.
                     }
                 }
                 exit(0); //terminate game.
@@ -177,9 +171,9 @@ bool level(int lvl_num) {  //main code for one level iteration
         return true; //else, simply return true.
     }
     free(outputIndx_Arr);
-    //outputIndx_Arr = NULL;
+    //outputIndx_Arr = NULL;  ***DELETE
     free(elements);
-    //elements = NULL;
+    //elements = NULL;   ***DELETE
 }
 
 int main(void)
@@ -234,6 +228,15 @@ int main(void)
         exit(0);   
     }
 #endif
+
+
+
+
+
+
+// THE REST OF THE CODE FROM HERE WERE JUST USED FOR DEVELOPING CONCEPTS FOR THE GAME.
+// IT HELPED FOR DEVELOPING KNOWLEDGE ON THE HARDWARE IN RESPECT TO WHAT WE TYPED IN FOR THE SOFTWARE.
+//Note some of the codes from the original starter code has been changed over the course of the development of the project.
 
 #ifdef BUTTON_BLINK
     // Wait for the user to push the blue button, then blink the LED.
@@ -332,136 +335,6 @@ int main(void)
         if (key == 3) // top-right key in a 4x4 keypad, usually 'A'
             HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);   // toggle LED on or off     
         while (ReadKeypad() >= 0);  // wait until key is released   
-    }
-#endif
-
-#ifdef SEVEN_SEGMENT
-    // Display the numbers 0 to 9 inclusive on the 7-segment display, pausing for a second between each one.
-    // (remember that the GND connection on the display must go through a 220 ohm current-limiting resistor!)
-    
-    Initialize7Segment();
-    while (true)
-        for (int i = 0; i < 10; ++i)
-        {
-            Display7Segment(i);
-            HAL_Delay(1000);  // 1000 milliseconds == 1 second
-        }
-#endif
-
-#ifdef KEYPAD_SEVEN_SEGMENT
-    // Combines the previous two examples, displaying numbers from the keypad on the 7-segment display.
-
-    // this string contains the symbols on the external keypad
-    // (they may be different for different keypads)
-    char *keypad_symbols = "123A456B789C*0#D";
-    // note that they're numbered from left to right and top to bottom, like reading words on a page
-
-    InitializeKeypad();
-    Initialize7Segment();
-    while (true)
-    {
-        int key = ReadKeypad();
-        if (key >= 0)
-            Display7Segment(keypad_symbols[key]-'0');  // tricky code to convert ASCII digit to a number
-    }
-#endif
-
-#ifdef COLOR_LED
-    // Cycle through all 8 possible colors (including off and white) as the on-board button is pressed.
-    // This example assumes that the color LED is connected to pins D11, D12 and D13.
-
-    // Remember that each of those three pins must go through a 220 ohm current-limiting resistor!
-    // Also remember that the longest pin on the LED should be hooked up to GND.
-
-    InitializePin(GPIOA, GPIO_PIN_7, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, 0);  // initialize color LED output pins
-    while (true) {
-        for (int color = 0; color < 8; ++color) {
-            // bottom three bits indicate which of the three LEDs should be on (eight possible combinations)
-            while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));   // wait for button press
-            if(color%3 == 0) {
-                HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, color & 0x04);  // blue  (hex 1 == 0001 binary)
-            } else if (color%3 == 1) {
-                HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, color & 0x06);  // green (hex 2 == 0010 binary)
-            } else {
-                HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, color & 0x02);  // red   (hex 4 == 0100 binary)
-            }
- 
-            while (!HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // wait for button release
-        }
-    }
-#endif
-
-#ifdef ROTARY_ENCODER
-    // Read values from the rotary encoder and update a count, which is displayed in the console.
-
-    InitializePin(GPIOB, GPIO_PIN_5, GPIO_MODE_INPUT, GPIO_PULLUP, 0);   // initialize CLK pin
-    InitializePin(GPIOB, GPIO_PIN_4, GPIO_MODE_INPUT, GPIO_PULLUP, 0);   // initialize DT pin
-    InitializePin(GPIOB, GPIO_PIN_10, GPIO_MODE_INPUT, GPIO_PULLUP, 0);  // initialize SW pin
-    
-    bool previousClk = false;  // needed by ReadEncoder() to store the previous state of the CLK pin
-    int count = 0;             // this gets incremented or decremented as we rotate the encoder
-
-    while (true)
-    {
-        int delta = ReadEncoder(GPIOB, GPIO_PIN_5, GPIOB, GPIO_PIN_4, &previousClk);  // update the count by -1, 0 or +1
-        if (delta != 0) {
-            count += delta;
-            char buff[100];
-            sprintf(buff, "%d  \r", count);
-            SerialPuts(buff);
-        }
-        bool sw = !HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_10);  // read the push-switch on the encoder (active low, so we invert it using !)
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, sw);  // turn on LED when encoder switch is pushed in
-    }
-#endif
-
-#ifdef ANALOG
-    // Use the ADC (Analog to Digital Converter) to read voltage values from two pins.
-
-    __HAL_RCC_ADC1_CLK_ENABLE();        // enable ADC 1
-    ADC_HandleTypeDef adcInstance;      // this variable stores an instance of the ADC
-    InitializeADC(&adcInstance, ADC1);  // initialize the ADC instance
-    // Enables the input pins
-    // (on this board, pin A0 is connected to channel 0 of ADC1, and A1 is connected to channel 1 of ADC1)
-    InitializePin(GPIOA, GPIO_PIN_0 | GPIO_PIN_1, GPIO_MODE_ANALOG, GPIO_NOPULL, 0);   
-    while (true)
-    {
-        // read the ADC values (0 -> 0V, 2^12 -> 3.3V)
-        uint16_t raw0 = ReadADC(&adcInstance, ADC_CHANNEL_0);
-        uint16_t raw1 = ReadADC(&adcInstance, ADC_CHANNEL_1);
-
-        // print the ADC values
-        char buff[100];
-        sprintf(buff, "Channel0: %hu, Channel1: %hu\r\n", raw0, raw1);  // hu == "unsigned short" (16 bit)
-        SerialPuts(buff);
-    }
-#endif
-
-#ifdef PWM
-    // Use Pulse Width Modulation to fade the LED in and out.
-    uint16_t period = 100, prescale = 16;
-
-    __TIM2_CLK_ENABLE();  // enable timer 2
-    TIM_HandleTypeDef pwmTimerInstance;  // this variable stores an instance of the timer
-    InitializePWMTimer(&pwmTimerInstance, TIM2, period, prescale);   // initialize the timer instance
-    InitializePWMChannel(&pwmTimerInstance, TIM_CHANNEL_1);          // initialize one channel (can use others for motors, etc)
-
-    InitializePin(GPIOA, GPIO_PIN_5, GPIO_MODE_AF_PP, GPIO_NOPULL, GPIO_AF1_TIM2); // connect the LED to the timer output
-
-    while (true)
-    {
-        // fade the LED in by slowly increasing the duty cycle
-        for (uint32_t i = 0; i < period; ++i)
-        {
-            SetPWMDutyCycle(&pwmTimerInstance, TIM_CHANNEL_1, i);
-            HAL_Delay(5);
-        }
-        // fade the LED out by slowly decreasing the duty cycle
-        for (uint32_t i = period; i > 0; --i)
-        {
-            SetPWMDutyCycle(&pwmTimerInstance, TIM_CHANNEL_1, i);
-            HAL_Delay(5);
-        }
     }
 #endif
     return 0;
